193704c7d06487b7260f486ed9dc8d1447c83095
Eliezer.BWANA <Eliezer.BWANA@congo-equipment.com>
2025-05-19 16:24:15 +0200

    Add API methods and update constants for API endpoint
    Updated `IApiService.cs` to include methods for saving raw clocking data, saving recordings, and retrieving employee information using the Refit library. Added necessary namespaces.
    
    Introduced a new constant `DB_API` in `Constants.cs` to define the base URL for the API, facilitating easier management of the endpoint.

---
+++ NfcReader/Services/Interfaces/IApiService.cs
@@ -0,0 +1,16 @@
+﻿using NfcReader.Models;
+using Refit;
+
+namespace NfcReader.Services.Interfaces;
+
+public interface IApiService
+{
+    [Post("/clocking/save-raw")]
+    Task<ApiResponse<bool>> SaveRawClocking([Body] RawClocking clocking);
+
+    [Post("/clocking/save-recording")]
+    Task<ApiResponse<string>> SaveRecording([Body] Recording recording);
+
+    [Get("/clocking/employee-info/{badgeId}")]
+    Task<ApiResponse<Employee>> GetEmployeeInfo(string badgeId);
+}
--- NfcReader/Utils/Constants.cs
+++ NfcReader/Utils/Constants.cs
@@ -5,5 +5,7 @@
         public static string BD_PATH = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
         public static string DB_NAME = "nfcReader.db";
         public static string DB_PATH = Path.Combine(BD_PATH, DB_NAME);
+        public static string DB_API = "http://localhost:5000/api";
+
     }
 }
