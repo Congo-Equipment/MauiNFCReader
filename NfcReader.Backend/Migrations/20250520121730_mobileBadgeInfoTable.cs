126cb683af79e857a437d28f697c0be6933e5c8d
Eliezer.BWANA <Eliezer.BWANA@congo-equipment.com>
2025-05-20 14:21:15 +0200

    Add mobileBadgeInfoTable migration and update models
    This commit introduces a new migration named `mobileBadgeInfoTable`, which creates the `T_MOBILE_BADGE_INFO` table with columns for `Id`, `BadgeId`, `Created`, and `StaffId`. The `Id` column is set as the primary key. Additionally, the migration updates the `Employee` and `RawClocking` entities to include new properties and corrects the column name for `Department` in the `Employee` entity. The model snapshot is also updated to reflect these changes, ensuring consistency in the database schema.

---
+++ NfcReader.Backend/Migrations/20250520121730_mobileBadgeInfoTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NfcReader.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class mobileBadgeInfoTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "T_MOBILE_BADGE_INFO",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    BadgeId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    StaffId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_T_MOBILE_BADGE_INFO", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "T_MOBILE_BADGE_INFO");
+        }
+    }
+}
