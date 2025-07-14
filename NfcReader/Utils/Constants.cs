2d1052c35e1f15aae97d5811262a5d70e24f0083
Eliezer.BWANA <Eliezer.BWANA@congo-equipment.com>
2025-07-14 12:08:46 +0200

    color for snackbar

--- NfcReader/Utils/Constants.cs
+++ NfcReader/Utils/Constants.cs
@@ -1,12 +1,35 @@
-﻿namespace NfcReader.Utils
+﻿using CommunityToolkit.Maui.Core;
+
+namespace NfcReader.Utils
 {
     public class Constants
     {
         public static string BD_PATH = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
         public static string DB_NAME = "nfcReader.db";
         public static string DB_PATH = Path.Combine(BD_PATH, DB_NAME);
+        public static string SYS_PASSWORD = "sys.Adm1n.";
+        public static SnackbarOptions SnackbarSucceesStyle = new SnackbarOptions
+        {
+            Font = Microsoft.Maui.Font.Default,
+            BackgroundColor = Color.FromRgba(254,185,16,1),
+            TextColor = Colors.White
+        };
+       
+        public static SnackbarOptions SnackbarFailedStyle = new SnackbarOptions
+        {
+            Font = Microsoft.Maui.Font.Default,
+            BackgroundColor = Color.FromRgba(220,53,69,1),
+            TextColor = Colors.White
+        };
+        
+        public static SnackbarOptions SnackbarDefaultStyle = new SnackbarOptions
+        {
+            Font = Microsoft.Maui.Font.Default,
+            BackgroundColor = Color.FromRgba(120,120,120,1),
+            TextColor = Colors.Black
+        };
 #if DEBUG
-         public static string BASE_API = "http://10.0.15.57:1010/api";//dev
+        public static string BASE_API = "http://10.0.15.57:1010/api";//dev
         //public static string BASE_API = "http://10.0.15.177:1010/api";//prod
 #elif RELEASE
         public static string BASE_API = "http://10.0.15.177:1010/api";//prod
--- NfcReader/ViewModels/SettingsPageViewModel.cs
+++ NfcReader/ViewModels/SettingsPageViewModel.cs
@@ -1,4 +1,4 @@
-﻿using AsyncAwaitBestPractices;
+﻿using CommunityToolkit.Maui.Alerts;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using NfcReader.Models;
@@ -52,17 +52,26 @@ namespace NfcReader.ViewModels
 
                 IsBusy = false;
 
-                await AppShell.Current.DisplayAlert("NFC", response.Message, "OK");
+                await AppShell.Current.DisplaySnackbar(response.Message ?? "Succeeded", duration: TimeSpan.FromSeconds(3), visualOptions: Utils.Constants.SnackbarSucceesStyle);
             }
             catch (Exception ex)
             {
-                await AppShell.Current.DisplayAlert("NFC", ex.Message, "OK");
+                IsBusy = false;
+                await AppShell.Current.DisplaySnackbar(ex.Message ?? "Failed", duration: TimeSpan.FromSeconds(3), visualOptions: Utils.Constants.SnackbarFailedStyle);
             }
         }
 
         [RelayCommand]
         async Task ClearData()
         {
+            var prompt = await AppShell.Current.DisplayPromptAsync("NFC", "Enter the password to clear all data:", "Clear", "Cancel", "Password", -1, Keyboard.Text, "");
+
+            if (prompt != null && !prompt.Equals(Utils.Constants.SYS_PASSWORD))
+            {
+                await AppShell.Current.DisplaySnackbar("Incorrect password. Please try again.", duration: TimeSpan.FromSeconds(3), visualOptions: Utils.Constants.SnackbarDefaultStyle);
+                return;
+            }
+
             var canClear = await AppShell.Current.DisplayAlert("NFC", "Are you sure you want to clear all data?", "Yes", "No");
             if (!canClear)
                 return;
@@ -78,11 +87,12 @@ namespace NfcReader.ViewModels
 
                 IsBusy = false;
 
-                await AppShell.Current.DisplayAlert("NFC", result.Message, "OK");
+                await AppShell.Current.DisplaySnackbar(result.Message ?? "Succeeded", duration: TimeSpan.FromSeconds(3), visualOptions: Utils.Constants.SnackbarSucceesStyle);
             }
             catch (Exception ex)
             {
-                await AppShell.Current.DisplayAlert("NFC", ex.Message, "OK");
+                IsBusy = false;
+                await AppShell.Current.DisplaySnackbar(ex.Message ?? "Operation Failed", duration: TimeSpan.FromSeconds(3), visualOptions: Utils.Constants.SnackbarFailedStyle);
             }
         }
 
